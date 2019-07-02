a68d1ebdf5e90ac4fbe893e5651ce042f5498ded
acolytec3 <a@b.com>
2019-07-02 18:54:27 +0000

    Add ethereum address argument

--- ens-explorer.py
+++ ens-explorer.py
@@ -4,15 +4,17 @@ import requests
 import argparse
 
 parser = argparse.ArgumentParser(description="Learn about an ETH address")
-parser.add_argument('--ens', help='An ENS address')
-parser.add_argument('--provider', help='Your web3 provider', default='https://mainnet.infura.io')
+parser.add_argument('--ens', '-e', help='An human readable Ethereum address with a .eth extension')
+parser.add_argument('--address', '-a', help='An Ethereum address of the form 0x1a2b3c...')
+parser.add_argument('--provider', help='A preferred web3 provider for interacting with the blockchain; defaults to infura mainnet', default='https://mainnet.infura.io')
 args = parser.parse_args()
 
-w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io'))
-ns = ENS.fromWeb3(w3)
+w3 = Web3(Web3.HTTPProvider(args.provider))
+if args.ens:
+    ns = ENS.fromWeb3(w3)
+    eth_address = ns.address(args.ens)
+if args.address:
+    eth_address = args.address
 
-eth_address = ns.address(args.ens)
-
-print(eth_address)
 r = requests.get(f'https://api.aleth.io/v1/accounts/{eth_address}')
 print(w3.fromWei(int(r.json()['data']['attributes']['balance']),'ether'))
