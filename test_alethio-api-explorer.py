e341134177a59172bd1310559d870ceecdb6b15e
acolytec3 <a@b.com>
2019-07-03 18:36:18 +0000

    Add test skeleton

---
+++ test_alethio-api-explorer.py
@@ -0,0 +1,11 @@
+import unittest
+import pytest
+
+class testAlethioApi(unittest.TestCase):
+    def test_getEthBalance(self):
+        """Test getEthBalance method of alethioApi class. """
+        pass
+
+    def test_getERC20Balance(self):
+        """Test getERC20Balance method alethioApi class. """
+        pass
