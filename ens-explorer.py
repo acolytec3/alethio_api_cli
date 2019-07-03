c2f48fbf4f366dff5f8b79a5c4770f79e4d1d8d1
acolytec3 <a@b.com>
2019-07-03 17:47:53 +0000

    Abstract alethio API into class

--- ens-explorer.py
+++ ens-explorer.py
@@ -3,21 +3,37 @@ from web3 import Web3
 import requests
 import argparse
 
+class alethioAPI:
+    """Alethio API wrapper."""
+
+    def __init__(self, provider):
+        """ Initialize instance of alethioAPI. """
+        self.w3 = Web3(Web3.HTTPProvider(provider))
+    
+    def getEthBalance(self, ethAddress):
+        """ Get Ether Balance for current address."""
+        response = requests.get(f'https://api.aleth.io/v1/accounts/{ethAddress}')
+        return self.w3.fromWei(int(response.json()['data']['attributes']['balance']),'ether')
+
+    def getERC20Balances(self, ethAddress):
+        """ Get ERC-20 token balances for current address. """
+        response = requests.get('https://blockscout.com/eth/mainnet/api?module=account&action=tokenlist&address='+ethAddress)
+        return response.json()['result']
+    
+
 parser = argparse.ArgumentParser(description="Learn about an ETH address")
-parser.add_argument('--ens', '-e', help='A human readable Ethereum address with a .eth extension from ENS')
 parser.add_argument('--address', '-a', help='An Ethereum address of the form 0x1a2b3c...')
 parser.add_argument('--provider', help='A preferred web3 HTTP provider for interacting with the blockchain; defaults to infura mainnet', default='https://mainnet.infura.io')
+parser.add_argument('--ens', help='A human readable Ethereum address based on the ENS')
 args = parser.parse_args()
 
-w3 = Web3(Web3.HTTPProvider(args.provider))
+api = alethioAPI(args.provider)
 if args.ens:
-    ns = ENS.fromWeb3(w3)
-    eth_address = ns.address(args.ens)
-if args.address:
-    eth_address = args.address
-
-alethio_response = requests.get(f'https://api.aleth.io/v1/accounts/{eth_address}')
-blockscout_response = requests.get('https://blockscout.com/eth/mainnet/api?module=account&action=tokenlist&address='+eth_address)
-print('Ether balance: '+ str(w3.fromWei(int(alethio_response.json()['data']['attributes']['balance']),'ether')))
-for token in blockscout_response.json()['result']:
-    print('Symbol: ' + token['symbol'] + ', Token: ' + token['name'] + ', Amount: ' + str(w3.fromWei(int(token['balance']),'ether')))
+    ns = ENS.fromWeb3(api.w3)
+    ethAddress = ns.address(args.ens)
+elif args.address:
+    ethAddress = args.address
+    
+print('Ether balance: '+ str(api.getEthBalance(ethAddress)))
+for token in api.getERC20Balances(ethAddress):
+    print('Symbol: ' + token['symbol'] + ', Token: ' + token['name'] + ', Amount: ' + str(api.w3.fromWei(int(token['balance']),'ether')))
