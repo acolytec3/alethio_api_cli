e78461e71ff62f9e986e8964ff9c469310c839fa
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2019-07-05 14:10:45 -0400

    Add token transaction lists

--- interactive-explorer.py
+++ interactive-explorer.py
@@ -7,11 +7,17 @@ api = alethioApi.alethioAPI(loggingLevel='INFO', token='')
 
 session = PromptSession()
 
+def trxnDetails():
+    """Print transaction details for given transaction. """
+    trxnHash = session.prompt('Enter transaction hash: ')
+    print('Transaction details: ' + str(api.getTransactionDetails(trxnHash)['attributes']['msgPayload']['funcName']))
+ 
+
 while 1:
     choice = session.prompt('(a)ddress, (t)ransaction hash, (q)uit: ')
     if choice == 'a':
         ethAddress = session.prompt('Enter address: ')
-        choice = session.prompt('(b)alance, (t)oken balances, (e)ther transfers, (to)ken transfers')
+        choice = session.prompt('(b)alance, (t)oken balances, (e)ther transfers, (to)ken transfers: ')
         if choice == 'b':
             print('Ether balance: '+ str(api.getEthBalance(ethAddress)))
         if choice == 't':
@@ -20,18 +26,24 @@ while 1:
                     print('Symbol: ' + token['symbol'] + ', Token: ' + token['name'] + ', Amount: ' + token['balance'])
                 else:
                     print('Contract: ' + token['contractAddress'] + ', Amount: ' + token['balance'])
+            choice = session.prompt('(s)ee token transfers associated with specific token or (m)ain menu? ')
+            if choice == 's':
+                choice = session.prompt('Please enter the symbol or token address you want to see transfers for: ')
+                transfers = api.getTokenTransfers(ethAddress)
+                for trxn in transfers:
+                    if trxn['attributes']['symbol'] == choice:
+                        print('Amount: ' + str(api.w3.fromWei(int(trxn['attributes']['value']),'ether')) + ' Token: ' + trxn['attributes']['symbol'])
+                    elif trxn['relationships']['token']['data']['id'] == choice:
+                        print('Contract: ' + token['contractAddress'] + ', Amount: ' + token['balance'])
         if choice == 'e':
             for trxn in api.getEthTransfers(ethAddress):
                 print('Amount: ' + trxn['attributes']['total'])
         if choice == 'to':
             for trxn in api.getTokenTransfers(ethAddress):
                 print('Amount: ' + str(api.w3.fromWei(int(trxn['attributes']['value']),'ether')) + ' Token: ' + trxn['attributes']['symbol'])
-
-    if choice == 't':
-        trxnHash = session.prompt('Enter transaction hash: ')
-        print('Transaction details: ' + str(api.getTransactionDetails(trxnHash)['attributes']['msgPayload']['funcName']))
-        
-    if choice == 'q':
+    elif choice == 't':
+        trxnDetails()
+    elif choice == 'q':
         break
 
 
